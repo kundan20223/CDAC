6b1fb24219aee1eb5436cb6e4f0fa9f528e72169
kundan kumar <84066058+kundan20223@users.noreply.github.com>
2024-09-28 18:47:23 +0530

    Add files via upload

---
+++ ADS ASSIGNMENT/ASSIGNMENT-1/Program3.java
@@ -0,0 +1,19 @@
+
+public class Program3 {
+	static int fact(int n) {
+		if(n<=1) 
+			return 1;
+		
+		else 
+		  return  n*fact(n-1);
+		}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(fact(5));
+	}
+
+}
