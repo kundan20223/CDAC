8c104c0e4897379c7fca498e7a93fac2494526e8
kundan kumar <84066058+kundan20223@users.noreply.github.com>
2024-09-28 18:45:42 +0530

    Add files via upload

---
+++ ADS ASSIGNMENT/ASSIGNMENT-1/sqroot.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+public class sqroot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter number: ");
+		
+		double n=sc.nextInt();
+		
+		double y=  Math.sqrt(n);
+		
+		System.out.println(y);
+
+	}
+
+}
